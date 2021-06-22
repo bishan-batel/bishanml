826c7b0f868cdb7e5d96191c4cdc5d14c0eb9146
Kishan <bishan.batel@protonmail.com>
2021-06-22 01:18:40 -0500

    Updated version and download link

--- src/pages/goosepack/goosepack.tsx
+++ src/pages/goosepack/goosepack.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./goosepack.scss";
 
 export default (() => {
-  const downloadLink = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+  const downloadLink = "https://www.dropbox.com/s/8mej9j0orh1fujo/Southbrit%20Goose%20Pack%20v0.3.zip?dl=1";
 
   return (
     <div className="goosepack">
@@ -10,13 +10,16 @@ export default (() => {
       <div className="pack-details">
         <p>
           This texturepack is for use for all current and upcoming games made by
-          Southbrit Goose Games for Vibrant comunity server
+          Southbrit Goose Games for Vibrant comunity server. You <b>need optifine 
+            or a minecraft client that has optifine embeded such as BL or Lunar.</b>
+            <br />
+          You do not need to redownload
         </p>
 
         <div className="download">
           <a href={downloadLink}>Download</a>
           <h2 className="version">
-            <b>v1.0</b>
+            <b>v0.3</b>
           </h2>
         </div>
       </div>
